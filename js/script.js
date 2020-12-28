9b4aaf86e6f880547f9f07bcb0e71125162527ef
yudhaapermana <pyudha0902@gmail.com>
2020-12-28 23:49:45 +0700

    mengubah html, css dan menambahkan javascript

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.sr = ScrollReveal()
+
+sr.reveal('.content-text', {
+    duration: 2000,
+    origin: 'top',
+    distance: '100px',
+    delay: 100
+});
+
+sr.reveal('.title', {
+    duration: 1500,
+    origin: 'left',
+    distance: '100px',
+    delay: 100
+});
+
+sr.reveal('.5', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '100px',
+    delay: 100
+});
+
